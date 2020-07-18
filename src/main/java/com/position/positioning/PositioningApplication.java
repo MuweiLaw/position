0d1eb030caf94f97b57b87f2b99beabc6a8c40d2
murray <muweilaw@qq.com>
2020-07-18 10:29:04 +0800

    项目雏形

---
+++ src/main/java/com/position/positioning/PositioningApplication.java
@@ -0,0 +1,13 @@
+package com.position.positioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositioningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PositioningApplication.class, args);
+    }
+
+}
